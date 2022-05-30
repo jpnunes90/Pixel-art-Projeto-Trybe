7d76ddbd4e924fc758e56da33ba0f4da63b9e14c
jpnunes90 <jp-nunes@hotmail.com>
2022-05-30 15:53:19 -0300

    inicio da implementação da função para criação da paleta de cores com cores aleatorias

---
+++ script.js
@@ -0,0 +1,15 @@
+// let colorPalette = document.getElementById('color-palette');
+
+function paletteCreation() {
+    let colorPalette = document.getElementById('color-palette');
+    let newElement = document.createElement('div');
+    newElement.className = 'color';
+    colorPalette.appendChild(newElement)
+    
+}
+paletteCreation()
+
+
+function addElementChild (parent, element) {
+
+}
