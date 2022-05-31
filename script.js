961dd948b30ac4527ef43b23c1321bb740c634a1
jpnunes90 <jp-nunes@hotmail.com>
2022-05-31 13:05:28 -0300

    correção de bugs ate o requisito 6

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 // cria a paleta de cores
 let color = ['black', 'red', 'green', 'blue'];
-for(let index = 0; index < color.length; index += 1) {
-    paletteCreation(color[index]); 
+for (let index = 0; index < color.length; index += 1) {
+    if(color[index] === 'black') {
+        paletteCreation(color[index]).classList.add('selected');
+    } else {
+        paletteCreation(color[index]);
+    }
 }
 
 // cria painel de pixels
-for(let index = 1; index <= 25; index += 1) {
+for (let index = 1; index <= 25; index += 1) {
     pixelBoardCreation();
 }
 
@@ -14,15 +18,13 @@ for(let index = 1; index <= 25; index += 1) {
 function paletteCreation(color) {
     let colorPalette = document.getElementById('color-palette');
     let newElement = document.createElement('div');
-    newElement.addEventListener('click', function(){
-        let target = event.target;
-        console.log(target);
+    newElement.addEventListener('click', function () {
         let selecao = document.getElementsByClassName('selected');
-        for(let index = 0; index < selecao.length; index += 1) {
+        for (let index = 0; index < selecao.length; index += 1) {
             selecao[index].classList.remove('selected');
         }
         newElement.classList.add('selected');
-      });
+    });
     newElement.className = 'color';
     newElement.classList.add(color);
     colorPalette.appendChild(newElement);
